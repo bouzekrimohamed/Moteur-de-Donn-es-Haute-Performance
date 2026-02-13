7ffcef197e1a09c28699becba4ceb3206d5af208
AbelFEL <feliciaggiabel@gmail.com>
2026-02-13 16:00:49 +0100

    Création de la structure de données

--- Data/src/data/Colonne.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Colonne<T> {
-	private String name;
-	private List<T> data= new ArrayList<>();
-}
--- Data/src/data/Table.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Table {
-	private String name;
-	private List<Colonne> colonnes = new ArrayList<>();
-}
